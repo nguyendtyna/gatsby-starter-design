c3d8f9332502c940f0a680d53d4f14a49977a2f5
Tyna Nguyen <ngtyna@gmail.com>
2021-02-23 13:58:48 -0600

    Structure hero section component with styled components

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,18 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 function HeroSection() {
   return (
-    <div>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Design and code React apps</h1>
-      <p>
-        Don't skip design. Learn design and code, by building real apps with
-        React and Swift. Complete courses about the best tools.
-      </p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </div>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            Design
+            <br /> and code React apps
+          </Title>
+          <Description>
+            Don't skip design. Learn design and code, by building real apps with
+            React and Swift. Complete courses about the best tools.
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  padding: 200px 30px;
+  margin: 0 auto;
+`
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`
