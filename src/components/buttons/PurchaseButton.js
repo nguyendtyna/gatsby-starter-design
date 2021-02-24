34c2e14060a48d1b7d196292205acf0832c54a4a
Tyna Nguyen <ngtyna@gmail.com>
2021-02-24 15:58:09 -0600

    Style text within PurchaseButton component

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -11,8 +11,10 @@ export default function PurchaseButton(props) {
         <IconWrapper>
           <Icon src="/images/icons/credit.svg" />
         </IconWrapper>
-        <Title>{title || "Get Pro Access"}</Title>
-        <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        <TextWrapper>
+          <Title>{title || "Get Pro Access"}</Title>
+          <Subtitle>{subtitle || "$19 per month"}</Subtitle>
+        </TextWrapper>
       </Wrapper>
     </Link>
   )
@@ -27,6 +29,22 @@ const Wrapper = styled.div`
     0px 20px 40px rgba(23, 0, 102, 0.2),
     inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
   border-radius: 20px;
+  /* By default, grid is set to rows
+      - grid-template-columns allows for control of the size of each column
+  */
+  display: grid;
+  grid-template-columns: 53px auto;
+  /* Align content to the middle vertically:
+      - align-content works best with a single element smaller than the container size,
+      - align-items works best with multiple columns and multiple rows
+  */
+  align-items: center;
+  gap: 20px;
+`
+
+const TextWrapper = styled.div`
+  display: grid;
+  gap: 4px;
 `
 
 const Title = styled(Caption2)`
