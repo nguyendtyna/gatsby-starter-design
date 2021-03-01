466134bb6b03b7c7233ee77e9aad36e3c3c38492
Tyna Nguyen <ngtyna@gmail.com>
2021-02-28 19:13:46 -0600

    Select styled components, apply transitions and add graphical effects

--- src/components/buttons/PurchaseButton.js
+++ src/components/buttons/PurchaseButton.js
@@ -9,7 +9,7 @@ export default function PurchaseButton(props) {
     <Link to="/page-2">
       <Wrapper>
         <IconWrapper>
-          <Icon src="/images/icons/credit.svg" />
+          <Icon src="/images/icons/credit.svg" className="icon" />
           <Ring src="/images/icons/icon-ring.svg" />
         </IconWrapper>
         <TextWrapper>
@@ -36,11 +36,20 @@ const Wrapper = styled.div`
   gap: 20px;
   transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
 
+  /* An asterisk selects all child elements */
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
   :hover {
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
       0px 30px 60px rgba(23, 0, 102, 0.5),
       inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
     transform: translateY(-3px);
+    .icon {
+      transform: scale(1.2);
+    }
   }
 `
 
@@ -57,6 +66,7 @@ const Subtitle = styled(SmallText)`
   color: black;
   opacity: 0.7;
 `
+
 const Icon = styled.img`
   width: 29px;
   height: 29px;
@@ -66,6 +76,11 @@ const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+
+  /* Select the parent container with the hover interaction by name */
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
 
 const IconWrapper = styled.div`
@@ -78,4 +93,8 @@ const IconWrapper = styled.div`
   align-content: center;
   justify-self: center;
   position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg);
+  }
 `
