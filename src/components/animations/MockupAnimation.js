2baee388c695294d2f899339dce1af8f4ddd43e3
Tyna Nguyen <ngtyna@gmail.com>
2021-02-28 23:58:26 -0600

    Create a new MockupAnimation component containing div tags for mockups

---
+++ src/components/animations/MockupAnimation.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+export default function MockupAnimation() {
+  return (
+    <Wrapper>
+      <div className="mockup1" />
+      <div className="mockup2" />
+      <div className="mockup3" />
+      <div className="mockup4" />
+      <div className="mockup5" />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div``
